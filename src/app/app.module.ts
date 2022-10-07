a6763051b8a99c0375a55d536e64d0c60a8e751d
benja <devquirogalaspiurbenjamin@gmail.com>
2022-10-07 16:55:09 -0300

    Proyecto AP, frontend LOCAL, terminado para entregar, luego de modificarlo para pasarlo de local a produccion, faltan mejorar detalles, funcionalidades a futuro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,17 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { EditSkillComponent } from './components/hys/edit-skill.component';
+import { NewSkillComponent } from './components/hys/new-skill.component';
 
 @NgModule({
   declarations: [
@@ -26,13 +37,24 @@ import { FooterComponent } from './components/footer/footer.component';
     HysComponent,
     ProyectosComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent,
+    EditSkillComponent,
+    NewSkillComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+
+  providers: [interceptorProvider],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/service/persona.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { persona } from '../model/persona.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PersonaService {
+  //URL = 'http://localhost:8080/personas/';
+  URL = environment.URL + 'personas/';
+
+  constructor(private http: HttpClient) {}
+
+  public getPersona(): Observable<persona> {
+    return this.http.get<persona>(this.URL + 'traer/perfil');
+  }
+}
